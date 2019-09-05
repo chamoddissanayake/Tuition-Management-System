de0b6c9cf798a91f582f2555a81697bd8dd4a921
Piyumi Dayarathna <lbpmdayarathna@gmail.com>
2019-09-05 12:19:52 +0530

    insert part of feedback function 2

--- app/src/main/java/com/test/tuitionmanagementsystem/FeedBack.java
+++ app/src/main/java/com/test/tuitionmanagementsystem/FeedBack.java
@@ -8,14 +8,13 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
-
 public class FeedBack extends AppCompatActivity {
 
     EditText t1;
     EditText t2;
     EditText t3;
     EditText t4;
+    EditText t5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +25,11 @@ public class FeedBack extends AppCompatActivity {
         String sID = intent.getStringExtra("StudentID");
         String sName = intent.getStringExtra("sName");
 
-        t1 = findViewById(R.id.editTextID);
-        t2 = findViewById(R.id.editTextName);
-        t3 = findViewById(R.id.editTextSubject);
-        t4 = findViewById(R.id.editTextFeedback);
+        t1 = findViewById(R.id.editTextID2);
+        t2 = findViewById(R.id.editTextName2);
+        t3 = findViewById(R.id.editTextSubject2);
+        t4 = findViewById(R.id.editTextFeedback2);
+        t5 = findViewById(R.id.editTextFid2);
 
         t1.setText(sID);
         t2.setText(sName);
@@ -43,15 +43,17 @@ public class FeedBack extends AppCompatActivity {
         String msg2 = t2.getText().toString();
         String msg3 = t3.getText().toString();
         String msg4 = t4.getText().toString();
+        String msg5 = t5.getText().toString();
 
-        if(msg1.equals("")||msg2.equals("")||msg3.equals("")||msg4.equals("")){
+        if(msg1.equals("")||msg2.equals("")||msg3.equals("")||msg4.equals("")||msg5.equals("")){
             Toast.makeText(getApplicationContext(),"All fields must be filled.",Toast.LENGTH_SHORT).show();
         }else{
-            Intent intent01 = new Intent(FeedBack.this,UpdateDeleteFeedback.class);
+            Intent intent01 = new Intent(FeedBack.this, Feedbacknext.class);
             intent01.putExtra("FirstText",msg1);
             intent01.putExtra("SecondText",msg2);
             intent01.putExtra("ThirdText",msg3);
             intent01.putExtra("ForthText",msg4);
+            intent01.putExtra("FithText",msg5);
 
             startActivity(intent01);
         }
