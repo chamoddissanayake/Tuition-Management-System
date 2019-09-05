96acdfd13d1a4ebbe4f9499b010ce2b078302006
Hasini <vihangadias97@gmail.com>
2019-09-05 12:35:23 +0530

    Homework

--- app/src/main/java/com/test/tuitionmanagementsystem/Homework.java
+++ app/src/main/java/com/test/tuitionmanagementsystem/Homework.java
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Homework extends AppCompatActivity {
     EditText txtdesc;
     Button btnsubmithome;
     Button update,delete;
+
+    EditText txthomeworkID, txtsubName, txtHomeworkDescription;
+
+    DatabaseReference dbRef;
+    Homework_tbl hwtbl;
 
 
     @Override
@@ -22,20 +31,61 @@ public class Homework extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homework);
 
-        txtdesc = (EditText)findViewById(R.id.txtHomeworkDescription);
+        txthomeworkID = findViewById(R.id.txt_homeworkID);
+        txtsubName = findViewById(R.id.txt_subName);
+        txtHomeworkDescription = findViewById(R.id.txt_HomeworkDescription);
+
+        txtdesc = (EditText)findViewById(R.id.txt_HomeworkDescription);
         btnsubmithome = (Button)findViewById(R.id.btnSubmitHomework);
+
+        hwtbl = new Homework_tbl();
 
         btnsubmithome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                dbRef = FirebaseDatabase.getInstance().getReference().child("Homework_tbl");
+
+                /*try{
+
+                    if(TextUtils.isEmpty(txthomeworkID.getText().toString()))
+                        Toast.makeText(getApplicationContext(),"Please Enter homework ID",Toast.LENGTH_SHORT).show();
+                    else if(TextUtils.isEmpty(txtsubName.getText().toString()))
+                        Toast.makeText(getApplicationContext(),"Please Enter the subject name",Toast.LENGTH_SHORT).show();
+                    else if (TextUtils.isEmpty(txtHomeworkDescription.getText().toString()))
+                        Toast.makeText(getApplicationContext(),"Please Enter the description",Toast.LENGTH_SHORT).show();
+
+                    else{
+
+                        hwtbl.setHomeworkID(txthomeworkID.getText().toString().trim());
+                        hwtbl.setSubName(txtsubName.getText().toString().trim());
+                        hwtbl.setHoework(txtHomeworkDescription.getText().toString().trim());
+
+                        dbRef.push().setValue(hwtbl);
+
+                        Toast.makeText(getApplicationContext(),"Data saves successfully",Toast.LENGTH_SHORT).show();
+                        clearControls();
+
+
+                    }
+
+                }
+                catch (NumberFormatException e){
+                    Toast.makeText(getApplicationContext(),"Invalid ID",Toast.LENGTH_SHORT).show();
+                }*/
 
                 Intent itodo = new Intent(Homework.this,Todolist.class);
                 String nameval = txtdesc.getText().toString();
                 itodo.putExtra("Desc ",nameval);
                 startActivity(itodo);
                 finish();
+
+
+
             }
         });
+
+
         update = findViewById(R.id.btnUpdate);
         delete = findViewById(R.id.btnDelete);
 
@@ -53,7 +103,17 @@ public class Homework extends AppCompatActivity {
             }
         });
 
+
+
     }
+
+    private void clearControls(){
+        txthomeworkID.setText("");
+        txtsubName.setText("");
+        txtHomeworkDescription.setText("");
+    }
+
+
 
 
 
---
+++ app/src/main/java/com/test/tuitionmanagementsystem/Homework_tbl.java
@@ -0,0 +1,44 @@
+package com.test.tuitionmanagementsystem;
+
+public class Homework_tbl {
+
+    private String homeworkID;
+    private String subName;
+    private String hoework;
+    private String tID;
+
+    public Homework_tbl() {
+    }
+
+    public String getHomeworkID() {
+        return homeworkID;
+    }
+
+    public void setHomeworkID(String homeworkID) {
+        this.homeworkID = homeworkID;
+    }
+
+    public String getSubName() {
+        return subName;
+    }
+
+    public void setSubName(String subName) {
+        this.subName = subName;
+    }
+
+    public String getHoework() {
+        return hoework;
+    }
+
+    public void setHoework(String hoework) {
+        this.hoework = hoework;
+    }
+
+    public String gettID() {
+        return tID;
+    }
+
+    public void settID(String tID) {
+        this.tID = tID;
+    }
+}
