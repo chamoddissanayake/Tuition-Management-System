c6043dc077fccc5d985c7213ddeeb61ae06213dd
Hasini <vihangadias97@gmail.com>
2019-09-06 13:44:20 +0530

    view of homework interface

---
+++ app/src/main/java/com/test/tuitionmanagementsystem/homeworkdashboard.java
@@ -0,0 +1,39 @@
+package com.test.tuitionmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class homeworkdashboard extends AppCompatActivity {
+
+    Button uploadhomework, viewhomework;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homeworkdashboard);
+
+        uploadhomework = findViewById(R.id.btnuploadHomework);
+        viewhomework = findViewById(R.id.btnviewHomework);
+
+        uploadhomework.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i33 = new Intent(homeworkdashboard.this,Homework.class);
+            }
+        });
+
+        viewhomework.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i44 = new Intent(homeworkdashboard.this,view_of_homework.class);
+            }
+        });
+
+
+
+    }
+}
