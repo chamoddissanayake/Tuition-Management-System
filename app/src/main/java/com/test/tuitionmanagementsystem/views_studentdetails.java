cd637a2b236edb298b265d7edcaf236914e92b80
Chamod Dissanayake <dissanayakechamod@gmail.com>
2019-09-13 14:21:31 +0530

    Update Delete modified.

--- app/src/main/java/com/test/tuitionmanagementsystem/views_studentdetails.java
+++ app/src/main/java/com/test/tuitionmanagementsystem/views_studentdetails.java
@@ -4,17 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.lang.reflect.Array;
 
 public class views_studentdetails extends AppCompatActivity {
 
@@ -22,134 +29,184 @@ public class views_studentdetails extends AppCompatActivity {
     Button btnDelete;
     Button btnSearch;
 
+    EditText AdmissionNo, sName, ContactNo, Address;
+    EditText etSearchID;
+    String photo_Link;
+
+    ImageView userPhoto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_views_studentdetails);
 
+        AdmissionNo = (EditText) findViewById(R.id.etAdmissionNo);
+        sName = (EditText) findViewById(R.id.etsName);
+        ContactNo = (EditText) findViewById(R.id.etContactNo);
+        Address = (EditText) findViewById(R.id.etAddress);
 
-        final EditText t0 = findViewById(R.id.SearchID);
-        final EditText t2 = findViewById(R.id.etAdmissionNo);
-        final EditText t3 = findViewById(R.id.address);
-        final EditText t4 = findViewById(R.id.etContactNo);
-        final TextView t5 = findViewById(R.id.txtID);
         btnUpdate = (Button) findViewById(R.id.btnUpdate);
         btnDelete = (Button) findViewById(R.id.btnDelete);
         btnSearch = (Button) findViewById(R.id.btnSearch);
 
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        etSearchID = (EditText) findViewById(R.id.SearchID);
 
-                DatabaseReference updateRef = FirebaseDatabase.getInstance().getReference().child("StudentDetails");
-                updateRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                        StudentDetails_tb std = new StudentDetails_tb();
-
-                        if(dataSnapshot.hasChild(t0.getText().toString())){
-
-                            std.setStudentName(t2.getText().toString());
-                            std.setAddress(t3.getText().toString());
-                            std.setTel(t4.getText().toString());
-                            std.setAdmissionNo(t0.getText().toString());
-
-                            DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("StudentDetails").child(t0.getText().toString());
-                            dbRef.setValue(std);
-
-                            Toast.makeText(getApplicationContext(),"Data updated successfully!",Toast.LENGTH_SHORT).show();
-
-                        }
-                        else{
-                            Toast.makeText(getApplicationContext(),"No sourse to update",Toast.LENGTH_SHORT).show();
-
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
+        userPhoto = (ImageView)findViewById(R.id.userPhoto);
 
 
-            }
-        });
-
-        //delete function
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                DatabaseReference delRef = FirebaseDatabase.getInstance().getReference().child("StudentDetails");
-                delRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if(dataSnapshot.hasChild(t0.getText().toString())){
-                            DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("StudentDetails").child(t5.getText().toString());
-
-                            dbRef.removeValue();
-
-                            Toast.makeText(getApplicationContext(),"Data deleted succesfully",Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(getApplicationContext(),"No source to delete",Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-
-
-            }
-        });
-
-        //view button
-
+        AdmissionNo.setEnabled(false);
+        sName.setEnabled(false);
+        ContactNo.setEnabled(false);
+        Address.setEnabled(false);
 
 
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                DatabaseReference reafRef = FirebaseDatabase.getInstance().getReference().child("StudentDetails").child(t0.getText().toString());
-                reafRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        if (dataSnapshot.hasChildren()) {
-
-                            t2.setText(dataSnapshot.child("studentName").getValue().toString());
-                            t5.setText(dataSnapshot.child("admissionNo").getValue().toString());
-                            t3.setText(dataSnapshot.child("address").getValue().toString());
-                            t4.setText(dataSnapshot.child("tel").getValue().toString());
-
-                            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
-
-
-                        } else {
-                            Toast.makeText(getApplicationContext(), "No source to display", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
+                if(TextUtils.isEmpty(etSearchID.getText().toString()) ){
+                    Toast.makeText(getApplicationContext(),"Please enter Student ID for search",Toast.LENGTH_SHORT).show();
+                }else{
+                    //fetch data and fill in EditTexts
+                    fillStudentData();
+                    fillImage();
+                }
 
             }
         });
 
 
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(TextUtils.isEmpty(AdmissionNo.getText().toString())){
+
+                }else if(TextUtils.isEmpty(sName.getText().toString())){
+                    Toast.makeText(getApplicationContext(),"Please Enter Name",Toast.LENGTH_SHORT).show();
+                }else if(TextUtils.isEmpty(ContactNo.getText().toString())){
+                    Toast.makeText(getApplicationContext(),"Please Enter Contact Number",Toast.LENGTH_SHORT).show();
+                }else if(TextUtils.isEmpty(Address.getText().toString())){
+                    Toast.makeText(getApplicationContext(),"Please Enter Address",Toast.LENGTH_SHORT).show();
+                }else{
+                    //Update database
+//                    Toast.makeText(getApplicationContext(),""+photo_Link,Toast.LENGTH_LONG).show();
+                    updateStudentData();
+                }
+
+            }
+        });
 
 
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                deleteStudentDetails();
+
+            }
+        });
+
+    }
+
+    private void fillImage() {
+//"https://firebasestorage.googleapis.com/v0/b/tuitionmanagementsystem-1af02.appspot.com/o/StudentPhotos%2F1568363430716.jpg?alt=media&token=2cb3ee0f-cc71-4139-8130-a51743849b02"
+        Glide.with(this)
+                .load(photo_Link)
+                .into(userPhoto);
+
+    }
+
+    private void deleteStudentDetails() {
+        DatabaseReference delRef = FirebaseDatabase.getInstance().getReference().child("StudentDetails");
+        delRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.hasChild(AdmissionNo.getText().toString())){
+                    DatabaseReference dbRefDelete = FirebaseDatabase.getInstance().getReference().child("StudentDetails").child(AdmissionNo.getText().toString());
+
+                    dbRefDelete.removeValue();
+
+                    Toast.makeText(getApplicationContext(),"Data deleted succesfully",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"No source to delete",Toast.LENGTH_SHORT).show();
+                }
 
 
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void updateStudentData() {
+        DatabaseReference updateRef = FirebaseDatabase.getInstance().getReference().child("StudentDetails");
+        updateRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                StudentDetails_tb stdObj = new StudentDetails_tb();
+
+                        if(dataSnapshot.hasChild(AdmissionNo.getText().toString())){
+
+                            stdObj.setStudentName(sName.getText().toString());
+                            stdObj.setAddress(Address.getText().toString());
+                            stdObj.setTel(ContactNo.getText().toString());
+                            stdObj.setAdmissionNo(AdmissionNo.getText().toString());
+                            stdObj.setPhotoLink(photo_Link);
+
+                            DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference().child("StudentDetails").child(AdmissionNo.getText().toString());
+                            dbRef.setValue(stdObj);
+
+                            Toast.makeText(getApplicationContext(),"Data updated successfully!",Toast.LENGTH_SHORT).show();
+
+                        }else{
+                            Toast.makeText(getApplicationContext(),"No record",Toast.LENGTH_SHORT).show();
+                        }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void fillStudentData() {
+        DatabaseReference readRef = FirebaseDatabase.getInstance().getReference().child("StudentDetails").child(etSearchID.getText().toString());
+        readRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.hasChildren()){
+                    sName.setText(dataSnapshot.child("studentName").getValue().toString());
+                    AdmissionNo.setText(dataSnapshot.child("admissionNo").getValue().toString());
+                    Address.setText(dataSnapshot.child("address").getValue().toString());
+                    ContactNo.setText(dataSnapshot.child("tel").getValue().toString());
+                    photo_Link = dataSnapshot.child("photoLink").getValue().toString().trim();
+                    //Fetch Photo link
+
+                    AdmissionNo.setEnabled(false);
+                    sName.setEnabled(true);
+                    ContactNo.setEnabled(true);
+                    Address.setEnabled(true);
+                    Toast.makeText(getApplicationContext(), "Found", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Student Not found", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
