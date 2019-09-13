53a04f61467a92afbb615d1bdfe13a2cb68a22f0
Chamod Dissanayake <dissanayakechamod@gmail.com>
2019-09-13 09:24:24 +0530

    take camera input

--- app/src/main/java/com/test/tuitionmanagementsystem/StudentRegistration.java
+++ app/src/main/java/com/test/tuitionmanagementsystem/StudentRegistration.java
@@ -1,31 +1,54 @@
 package com.test.tuitionmanagementsystem;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class StudentRegistration extends AppCompatActivity {
 
     TextView tx1, tx2;
-//    EditText T1;
-//    EditText T2;
-//    EditText T3;
-//    EditText T4;
     EditText etsName, etAdmissionNo, etAddress, etContactNo, etPassword, etRePassword;
+    ImageView imgViewInputPhoto;
 
-    Button btnSave;
+    public Uri imguri;
+    StorageReference mStorageRef;
+
+    Button btnSave, btnTakePhoto, btnChoosePhoto;
     DatabaseReference dbRef;
+
+    private StorageTask uploadTask;
+    private String completeImagePath;
 
     String ID = "";
     String Name = "";
@@ -48,6 +71,11 @@ public class StudentRegistration extends AppCompatActivity {
         etContactNo = (EditText) findViewById(R.id.etContactNo);
         etPassword = (EditText) findViewById(R.id.etPassword);
         etRePassword = (EditText) findViewById(R.id.etRePassword);
+
+        btnTakePhoto = (Button) findViewById(R.id.btnTakePhoto);
+        btnChoosePhoto = (Button) findViewById(R.id.btnChoosePhoto);
+
+        imgViewInputPhoto= findViewById(R.id.imgViewInputPhoto);
 
         tx1 = findViewById(R.id.tNamelbl);
         tx2 = findViewById(R.id.tIDlbl);
@@ -123,6 +151,21 @@ public class StudentRegistration extends AppCompatActivity {
 
         });
 
+
+
+
+        btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkPermissionREAD_EXTERNAL_STORAGE()) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent,0);
+                }
+
+            }
+        });
+
+        //End of onCreate()
     }
 
     private void clearTextboxes() {
@@ -133,4 +176,103 @@ public class StudentRegistration extends AppCompatActivity {
         etPassword.setText("");
         etRePassword.setText("");
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//        if(requestCode==1 && resultCode==RESULT_OK && data !=  null && data.getData() != null){
+//            imguri = data.getData();
+//            imgViewInputPhoto.setImageURI(imguri);
+//
+//
+//
+//        }else
+        if(requestCode==0 ){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            imgViewInputPhoto.setImageBitmap(bitmap);
+            imguri = getImageUri(getApplicationContext(),bitmap);
+
+        }
+    }
+
+
+    private String getExtension(Uri uri){
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private Uri getImageUri(Context context, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
+
+
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE( ) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) this,
+                                    new String[] { Manifest.permission.WRITE_EXTERNAL_STORAGE },
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+    public void showDialog(final String msg, final Context context, final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // do your stuff
+                } else {
+//                    Toast.makeText(Login.this, "GET_ACCOUNTS Denied",
+//                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions,
+                        grantResults);
+        }
+    }
+
+
+
 }
